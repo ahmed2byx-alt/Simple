6e1575c7dbfc8817048a0d86ae6d2e957fa100de
Ahmed <Ahmed2byx@gmail.com>
2026-06-13 00:35:24 +0300

    update UI

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log ("saleem kalb")
+const welcomeWords = ["learn", "manage", "study" ,"Simple"]
+
+let wordNum = 0
+
+const welcome = document.getElementById("welcome")
+
+
+
+setInterval(() => {
+    wordNum = (wordNum + 1) % welcomeWords.length
+    welcome.innerHTML = welcomeWords[wordNum]
+}, 1000);
+
+
